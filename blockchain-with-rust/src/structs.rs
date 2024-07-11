4f8b050c8c910980a2f6532ec5b768a6db86a444
Mehran Khanjan <mehran@Mehrans-MacBook-Pro.local>
2024-07-11 13:29:47 +0400

    complete blockchain with rust

--- blockchain-with-rust/src/structs.rs
+++ blockchain-with-rust/src/structs.rs
@@ -1,3 +1,22 @@
+// --------------------------------------------------------------------------------------------------
+// Structs
+// --------------------------------------------------------------------------------------------------
+
+pub struct Block {
+    timestamp: i64,                
+    pre_block_hash: String,         
+    hash: String,                   
+    transactions: Vec<Transaction>, 
+    nonce: i64,                     
+    height: usize,                  
+}
+
+pub struct Transaction {
+    id: Vec<u8>,         
+    vin: Vec<TXInput>,   
+    vout: Vec<TXOutput>, 
+}
+
 pub struct TXInput {
     txid: Vec<u8>,      
     vout: usize,       
@@ -10,22 +29,28 @@ pub struct TXOutput {
     pub_key_hash: Vec<u8>,
 }
 
-pub struct Transaction {
-    id: Vec<u8>,         
-    vin: Vec<TXInput>,   
-    vout: Vec<TXOutput>, 
-}
-
-pub struct Block {
-    timestamp: i64,                
-    pre_block_hash: String,         
-    hash: String,                   
-    transactions: Vec<Transaction>, 
-    nonce: i64,                     
-    height: usize,                  
-}
-
 pub struct Blockchain {
     tip_hash: Arc<RwLock<String>>, // hash of last block
     db: Db,
+}
+
+pub struct Wallet {
+    pkcs8: Vec<u8>,
+    public_key: Vec<u8>,
+}
+
+pub struct Node {
+    addr: String,
+}
+pub struct Nodes {
+    inner: RwLock<Vec<Node>>,
+}
+
+pub struct Server {
+    blockchain: Blockchain,
+}
+
+pub struct ProofOfWork {
+    block: Block,
+    target: BigInt,
 }
